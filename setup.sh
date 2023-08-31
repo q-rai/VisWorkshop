7717765d7821cf414ae1864ef1cc76b73e76d53c
Andy Berres <q-rai@users.noreply.github.com>
2023-08-31 13:33:27 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copy all needed files
+
+#!/bin/bash
+
+# Install needed Python Packages
+
+
+# Copy all needed files
+
+echo "Fetching needed files .."
+mkdir -p Data
+curl -s -o Data/table.txt https://docs.google.com/spreadsheets/d/1h5kxUo2iTxa_L3-yszERP21ktYf8CxhmNUfoGOki5ow/edit?usp=sharing
+echo "Done"
