017ecf2415897df7008b492610c243b305a554dd
Andy Berres <andy.berres@nrel.gov>
2023-09-12 22:19:31 -0500

    Updated setup to pull more data

--- setup.sh
+++ setup.sh
@@ -11,4 +11,6 @@
 echo "Fetching needed files .."
 mkdir -p Data
 curl -s -o Data/table.csv https://raw.githubusercontent.com/q-rai/VisWorkshop/main/Data/smiley.csv
+curl -s -o Data/energy2021.csv https://raw.githubusercontent.com/q-rai/VisWorkshop/main/Data/energy2021.csv
+curl -s -o Data/energy1960-2021.csv https://raw.githubusercontent.com/q-rai/VisWorkshop/main/Data/energy1960-2021.csv
 echo "Done"
